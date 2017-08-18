b6fb00d66f846fe08118325679d84c4e73981141
Misael Batista Damasceno <misael19972008@hotmail.com>
2017-08-18 20:13:18 -0300

    quadrada_simetrica_lab03

--- lab03.cpp
+++ lab03.cpp
@@ -2,8 +2,49 @@
 #include <vector>
 
 using namespace std;
-void leitura_matriz(vector<vector<int> > &vtr1){
+
+
+void transpose  (vector<vector<int> > &vtr1,vector<vector<int> > &vtr2,int l,int c){
+		
+		for (int i = 0; i < l ; ++i){
+			for (int j = 0; j < c; ++j){
+				vtr2[j][i]=vtr1[i][j];
+
+			}
+		}
+
+
+}
+bool quadrada(vector<vector<int> > vtr1,int l,int c){
+	if(l==c){
+		return true;
+	}else {
+		return false;
+	}
+}
+bool simetrica(vector<vector<int> >vtr1,int l,int c){
+	if(l==c){
+		for (int i = 0; i < l ; ++i){
+			for (int j = 0; j < c; ++j){
+				if(vtr1[i][j]==vtr1[j][i]){
+
+				}
+				else{
+					return false;
+				}
+			}
+		}
+	}
+	else{
+		return false;
+	}
+	return true;
+}
+
+int main (){
+
 	int linhas,colunas;
+	vector<vector<int> > vtr1;
 	cout<<"Digite o numero de Linhas "<<endl;
 	cin >> linhas;	
 	cout<<"Digite o numero de colunas "<<endl;
@@ -12,28 +53,7 @@ void leitura_matriz(vector<vector<int> > &vtr1){
 		for (int j = 0; j < colunas; ++j){
 			cin>> vtr1[i][j];	
 
-		}
 
+		}
 	}
-
-}
-
-void transpose  (vector<vector<int> > &vtr1,vector<vector<int> > &vtr2){
-		int linhas,colunas;
-		cout<<"Digite o numero de Linhas "<<endl;
-		cin >> linhas;	
-		cout<<"Digite o numero de colunas "<<endl;
-		cin >> colunas;	
-		for (int i = 0; i < linhas ; ++i){
-			for (int j = 0; j < colunas; ++j){
-				vtr2[j][i]=vtr1[i][j];
-
-			}
-		}
-
-
-}
-
-int main (){
-
-}
+}	
